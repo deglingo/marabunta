b12efc0120fa5585db5c4017e17000a33dbbf4ad
deglingo <alfredeneumeuh AT laposte DOT net>
2014-08-07 17:23:24 +0200

    added the main loop

--- server/mbsmain.c
+++ server/mbsmain.c
@@ -1,6 +1,7 @@
 /* mbsmain.c -
  */
 
+#include <glib.h>
 #include <stdio.h>
 
 #include "server/mbsserver.h"
@@ -14,9 +15,12 @@ int main ()
 {
   MbsGame *game;
   MbsServer *server;
+  GMainLoop *loop;
   printf("marabunta-server: hello!\n");
   game = mbs_game_new();
   server = mbs_server_new();
   mbs_server_start(server);
+  loop = g_main_loop_new(NULL, FALSE);
+  g_main_loop_run(loop);
   return 0;
 }
