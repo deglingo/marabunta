52dede344aeffcef8a8ae9ab591b0f684bd61164
deglingo <alfredeneumeuh AT laposte DOT net>
2014-08-08 11:18:53 +0200

    created the client main loop

--- client-al/mbalmain.c
+++ client-al/mbalmain.c
@@ -11,10 +11,13 @@
  */
 int main ()
 {
+  GMainLoop *loop;
   MbcClient *cli;
   cli = mbc_client_new();
   printf("marabunta-al: hello!\n");
   mbc_client_connect(cli, "localhost", 6666);
   mbc_client_send(cli, "hola!");
+  loop = g_main_loop_new(NULL, FALSE);
+  g_main_loop_run(loop);
   return 0;
 }
