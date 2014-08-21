459c0801cad62323cc9b58dd4c02f16a4656050d
deglingo <ahmidmefes@gmail.com>
2014-08-21 22:23:50 +0200

    client-al: created a display

--- client-al/mbalmain.c
+++ client-al/mbalmain.c
@@ -13,9 +13,14 @@
 int main ( int argc,
            char **argv )
 {
+  ALLEGRO_DISPLAY *display;
   GMainLoop *loop;
   MbcClient *cli;
   al_init();
+  if (!(display = al_create_display(640, 480))) {
+    fprintf(stderr, "al_create_display() failed\n");
+    exit(1);
+  }
   cli = mbc_client_new();
   printf("marabunta-al: hello!\n");
   mbc_client_connect(cli, "localhost", 6666);
