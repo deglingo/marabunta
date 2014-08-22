ae1ca9bf901ccfdbd685546c0e1afba7ccbc61bc
deglingo <ahmidmefes@gmail.com>
2014-08-22 13:09:05 +0200

    client-al: registered the display event source

--- client-al/mbalmain.c
+++ client-al/mbalmain.c
@@ -15,7 +15,9 @@ typedef struct _AlSource AlSource;
 struct _AlSource
 {
   GSource g_source;
-  ALLEGRO_EVENT_QUEUE *al_queue;
+  ALLEGRO_EVENT_QUEUE *queue;
+  ALLEGRO_EVENT event;
+  unsigned int pending : 1;
 };
 
 
@@ -23,15 +25,24 @@ struct _AlSource
 static gboolean _al_source_prepare ( GSource *src,
                                      gint *timeout )
 {
-  *timeout = -1;
-  return FALSE;
+  AlSource *al = (AlSource *) src;
+  *timeout = 0;
+  if (al->pending)
+    return TRUE;
+  if (al_get_next_event(al->queue, &al->event)) {
+    al->pending = 1;
+    return TRUE;
+  } else {
+    return FALSE;
+  }
 }
 
 
 
 static gboolean _al_source_check ( GSource *src )
 {
-  return FALSE;
+  AlSource *al = (AlSource *) src;
+  return al->pending;
 }
 
 
@@ -40,6 +51,13 @@ static gboolean _al_source_dispatch ( GSource *src,
                                       GSourceFunc callback,
                                       gpointer data )
 {
+  AlSource *al = (AlSource *) src;
+  if (!al->pending) {
+    fprintf(stderr, "[fixme] no event to dispatch!\n");
+    return TRUE;
+  }
+  fprintf(stderr, "[todo] dispatch event: %d\n", al->event.type);
+  al->pending = 0;
   /* return FALSE if the source should be removed */ 
   return TRUE;
 }
@@ -62,7 +80,7 @@ static GSource *al_source_new ( void )
   };
   AlSource *source;
   source = (AlSource *) g_source_new(&funcs, sizeof(AlSource));
-  /* source->al_queue = al_create_event_queue(); */
+  source->queue = al_create_event_queue();
   return (GSource *) source;
 }
 
@@ -84,6 +102,8 @@ int main ( int argc,
     fprintf(stderr, "al_create_display() failed\n");
     exit(1);
   }
+  al_register_event_source(((AlSource *) al_source)->queue,
+                           al_get_display_event_source(display));
   cli = mbc_client_new();
   printf("marabunta-al: hello!\n");
   mbc_client_connect(cli, "localhost", 6666);
