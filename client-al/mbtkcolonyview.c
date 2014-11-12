c13082692ce5563737fd5e77ad5eb2bd5be9678d
deglingo <ahmidmefes@gmail.com>
2014-11-12 11:15:53 +0100

    fixed MbtkColonyView base class

--- client-al/mbtkcolonyview.c
+++ client-al/mbtkcolonyview.c
@@ -12,8 +12,8 @@
  */
 typedef struct _Private
 {
-  AltkWidget *title;
-  AltkWidget *task_table;
+  /* AltkWidget *title; */
+  /* AltkWidget *task_table; */
   GList *tasks;
   MbcProxy *colony;
 }
@@ -23,11 +23,31 @@ typedef struct _Private
 
 
 
+static void size_request ( AltkWidget *widget,
+                           AltkRequisition *req );
+static void foreach ( AltkWidget *widget,
+                      AltkForeachFunc func,
+                      gpointer data );
+
+
+
+/* mbtk_colony_view_class_init:
+ */
+static void mbtk_colony_view_class_init ( LObjectClass *cls )
+{
+  ALTK_WIDGET_CLASS(cls)->size_request = size_request;
+  ALTK_WIDGET_CLASS(cls)->foreach = foreach;
+}
+
+
+
 /* mbtk_colony_view_init:
  */
 static void mbtk_colony_view_init ( LObject *obj )
 {
   MBTK_COLONY_VIEW(obj)->private = g_new0(Private, 1);
+  ALTK_WIDGET(obj)->flags = ALTK_WIDGET_FLAG_NOWINDOW;
+  /* altk_widget_set_event_mask( */
 }
 
 
@@ -37,65 +57,85 @@ static void mbtk_colony_view_init ( LObject *obj )
 AltkWidget *mbtk_colony_view_new ( void )
 {
   AltkWidget *view;
-  Private *priv;
+  /* Private *priv; */
   l_trash_push();
   view = ALTK_WIDGET(MBTK_COLONY_VIEW_NEW(NULL));
-  priv = PRIVATE(view);
-  altk_box_set_orientation(ALTK_BOX(view), ALTK_VERTICAL);
-  priv->title = L_TRASH_OBJECT
-    (altk_label_new("TITLE"));
-  altk_box_pack_start(ALTK_BOX(view), priv->title, 0);
-  priv->task_table = L_TRASH_OBJECT
-    (altk_box_new(ALTK_VERTICAL));
-  altk_box_pack_start(ALTK_BOX(view), priv->task_table, 0);  
+  /* priv = PRIVATE(view); */
+  /* priv->title = L_TRASH_OBJECT */
+  /*   (altk_label_new("TITLE")); */
+  /* altk_box_pack_start(ALTK_BOX(view), priv->title, 0); */
+  /* priv->task_table = L_TRASH_OBJECT */
+  /*   (altk_box_new(ALTK_VERTICAL)); */
+  /* altk_box_pack_start(ALTK_BOX(view), priv->task_table, 0);   */
   l_trash_pop();
   return view;
 }
 
 
 
-static void _on_task_workers_notify ( MbcProxy *task,
-                                      AltkWidget *label )
+/* size_request:
+ */
+static void size_request ( AltkWidget *widget,
+                           AltkRequisition *req )
 {
-  /* [fixme] ?? */
-  gchar text[32];
-  sprintf(text, "%" G_GINT64_FORMAT, MBC_TASK_PROXY(task)->workers);
-  altk_label_set_text(ALTK_LABEL(label), text);
+  req->width = req->height = 8;
 }
 
 
 
-static void _create_task ( MbtkColonyView *view,
-                           MbcProxy *task )
+/* foreach:
+ */
+static void foreach ( AltkWidget *widget,
+                      AltkForeachFunc func,
+                      gpointer data )
 {
-  Private *priv = PRIVATE(view);
-  AltkWidget *box, *label, *prio, *workers;
-  gchar *value;
-  GList *l;
-  box = L_TRASH_OBJECT
-    (altk_box_new(ALTK_HORIZONTAL));
-  altk_box_pack_start(ALTK_BOX(priv->task_table), box, 0);
-  label = L_TRASH_OBJECT
-    (altk_label_new(MBC_TASK_PROXY(task)->name));
-  altk_box_pack_start(ALTK_BOX(box), label, 0);
-  prio = L_TRASH_OBJECT
-    (mbtk_priority_new(MBC_TASK_PROXY(task)->priority));
-  altk_box_pack_start(ALTK_BOX(box), prio, 0);
-  value = g_strdup_printf("%" G_GINT64_FORMAT, MBC_TASK_PROXY(task)->workers);
-  workers = L_TRASH_OBJECT
-    (altk_label_new(value));
-  g_free(value);
-  altk_box_pack_start(ALTK_BOX(box), workers, 0);
-  l_signal_connect(L_OBJECT(task),
-                   "notify", g_quark_from_string("workers"),
-                   (LSignalHandler) _on_task_workers_notify,
-                   workers,
-                   NULL);
-  for (l = MBC_TASK_PROXY(task)->children; l; l = l->next)
-    {
-      _create_task(view, l->data);
-    }
+  CL_DEBUG("[TODO]");
 }
+
+
+
+/* static void _on_task_workers_notify ( MbcProxy *task, */
+/*                                       AltkWidget *label ) */
+/* { */
+/*   /\* [fixme] ?? *\/ */
+/*   gchar text[32]; */
+/*   sprintf(text, "%" G_GINT64_FORMAT, MBC_TASK_PROXY(task)->workers); */
+/*   altk_label_set_text(ALTK_LABEL(label), text); */
+/* } */
+
+
+
+/* static void _create_task ( MbtkColonyView *view, */
+/*                            MbcProxy *task ) */
+/* { */
+/*   Private *priv = PRIVATE(view); */
+/*   AltkWidget *box, *label, *prio, *workers; */
+/*   gchar *value; */
+/*   GList *l; */
+/*   box = L_TRASH_OBJECT */
+/*     (altk_box_new(ALTK_HORIZONTAL)); */
+/*   altk_box_pack_start(ALTK_BOX(priv->task_table), box, 0); */
+/*   label = L_TRASH_OBJECT */
+/*     (altk_label_new(MBC_TASK_PROXY(task)->name)); */
+/*   altk_box_pack_start(ALTK_BOX(box), label, 0); */
+/*   prio = L_TRASH_OBJECT */
+/*     (mbtk_priority_new(MBC_TASK_PROXY(task)->priority)); */
+/*   altk_box_pack_start(ALTK_BOX(box), prio, 0); */
+/*   value = g_strdup_printf("%" G_GINT64_FORMAT, MBC_TASK_PROXY(task)->workers); */
+/*   workers = L_TRASH_OBJECT */
+/*     (altk_label_new(value)); */
+/*   g_free(value); */
+/*   altk_box_pack_start(ALTK_BOX(box), workers, 0); */
+/*   l_signal_connect(L_OBJECT(task), */
+/*                    "notify", g_quark_from_string("workers"), */
+/*                    (LSignalHandler) _on_task_workers_notify, */
+/*                    workers, */
+/*                    NULL); */
+/*   for (l = MBC_TASK_PROXY(task)->children; l; l = l->next) */
+/*     { */
+/*       _create_task(view, l->data); */
+/*     } */
+/* } */
 
 
 
@@ -105,16 +145,16 @@ void mbtk_colony_view_set_colony ( MbtkColonyView *view,
                                    MbcProxy *colony )
 {
   Private *priv = PRIVATE(view);
-  gchar *title;
+  /* gchar *title; */
   ASSERT(MBC_IS_COLONY_PROXY(colony));
   ASSERT(!priv->colony); /* [todo] */
   l_trash_push();
   priv->colony = l_object_ref(colony);
-  /* set title */
-  title = l_object_to_string(L_OBJECT(colony));
-  altk_label_set_text(ALTK_LABEL(priv->title), title);
-  g_free(title);
-  /* task list */
-  _create_task(view, MBC_COLONY_PROXY(colony)->top_task);
+  /* /\* set title *\/ */
+  /* title = l_object_to_string(L_OBJECT(colony)); */
+  /* altk_label_set_text(ALTK_LABEL(priv->title), title); */
+  /* g_free(title); */
+  /* /\* task list *\/ */
+  /* _create_task(view, MBC_COLONY_PROXY(colony)->top_task); */
   l_trash_pop();
 }
