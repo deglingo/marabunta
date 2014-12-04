a3ae8bbe1e70b3f3eda58eb7fd453c92d6ce224c
deglingo <ahmidmefes@gmail.com>
2014-12-04 15:24:48 +0100

    added the new marabunta program

---
+++ src/mbbase.h
@@ -0,0 +1,12 @@
+/* mbbase.h -
+ */
+
+#ifndef _MBBASE_H_
+#define _MBBASE_H_
+
+#include <los.h>
+#include <clog.h>
+
+
+
+#endif
---
+++ src/mbmain.c
@@ -0,0 +1,14 @@
+/* mbmain.c -
+ */
+
+#include "mbbase.h"
+
+
+
+/* main:
+ */
+gint main ()
+{
+  CL_DEBUG("hello!");
+  return 0;
+}
